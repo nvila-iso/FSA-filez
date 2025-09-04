d61670d7a74ea17406369ff43135825358d2eade
nvila-iso <nvila88@proton.me>
2025-09-04 17:47:47 -0400

    completed.

--- db/queries/folders.js
+++ db/queries/folders.js
@@ -13,7 +13,13 @@ export async function getFolders() {
 }
 
 export async function getFolder(id) {
-  const SQL = `SELECT * FROM folders WHERE id = $1`;
+  const SQL = `SELECT *, 
+  (
+    SELECT json_agg(files)
+    FROM files
+    WHERE files.folder_id = folders.id
+  ) AS files
+  FROM folders WHERE id = $1`;
   const { rows: folder } = await db.query(SQL, [id]);
   return folder[0];
 }
